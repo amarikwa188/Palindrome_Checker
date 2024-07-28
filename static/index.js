de3261ceaf524cf26a75f0d4ed3f61129605c227
Amarikwa <amarikwa188@gmail.com>
2024-07-28 18:41:13 +0100

    implemented javascript functions

--- static/index.js
+++ static/index.js
@@ -1,3 +1,25 @@
 function checkPalindrome(){
+    const string = document.getElementById("entry").value;
+    const flip = string.split('').reverse().join('');
     
+    if (string === flip){
+        validPalindrome();
+    }else{
+        invalidPalindrome();
+    }
+}
+
+function validPalindrome(){
+    document.getElementById("result").style.backgroundColor = "lightgreen";
+
+    const word = document.getElementById("entry").value;
+    document.getElementById("result-text").innerHTML = `Yes! ${word} is a palindrome.`;
+    
+}
+
+function invalidPalindrome(){
+    document.getElementById("result").style.backgroundColor = "lightcoral";
+
+    const word = document.getElementById("entry").value;
+    document.getElementById("result-text").innerHTML = `Nope! ${word} is not a palindrome.`;
 }
